602c64feaf8810acc2b0dfa59751b2d34a36ae68
pahc9 <pahc9@LAPTOP-UG979AH0>
2021-01-07 11:06:13 +0100

    opgave7

---
+++ Opgave7/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Opgave7
+{
+    class Program
+    {
+        public int start_point = 50;
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            string func = Console.ReadLine();
+
+            switch (func)
+            {
+                case "sin":
+                    p.makeSin();
+                    break;
+                case "cos":
+                    p.makeCos();
+                    break;
+                default:
+                    Console.WriteLine("Wrong input.");
+                    break;
+            }
+        }
+
+        void makeSin()
+        {
+            int row = 1; int column = 0; double rad = 0;
+            while(Console.ReadKey().Key != ConsoleKey.Q)
+            {
+                column = Convert.ToInt32(10 * Math.Sin(rad * Math.PI));
+                 
+                row++;
+
+                rad += 0.2;
+                Console.SetCursorPosition(column+start_point, row);
+                Console.WriteLine("*");
+            }
+        }
+
+        void makeCos()
+        {
+            int row = 1; int column = 0; double rad = 0;
+            while (Console.ReadKey().Key != ConsoleKey.Q)
+            {
+                column = Convert.ToInt32(10 * Math.Cos(rad * Math.PI));
+                row++;
+
+                rad += 0.2;
+                Console.SetCursorPosition(column + start_point, row);
+                Console.WriteLine("*");
+            }
+        }
+    }
+}
