58f7e1745405fb6a1e034853f0ecc27a228e1f30
pahc9 <pahc9@LAPTOP-UG979AH0>
2021-01-07 08:24:26 +0100

    opgave9

--- opgave9/Program.cs
+++ opgave9/Program.cs
@@ -7,7 +7,14 @@ namespace opgave9
     {
         static void Main(string[] args)
         {
-            
+            //Lav generisk liste med tre tal
+
+            List<int> samling = new List<int>();
+            samling.Add(5); samling.Add(3); samling.Add(9000);
+            foreach(int element in samling)
+            {
+                Console.WriteLine(element);
+            }
         }
     }
 }
